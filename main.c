fee2187ede0674491cd25781002141202c4de04f
Dongkyoo <wind.dong.dream@gmail.com>
2020-04-18 02:52:07 +0900

    프롬프트 출력

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "prompt.h"
 
 int main() {
-
+	printPrompt();
 	exit(0);
 }
--- prompt.h
+++ prompt.h
@@ -1,1 +1,8 @@
+#ifndef H_PROMPT
+#define H_PROMPT
+
 #define STUDENT_ID "20162489"
+
+void printPrompt();
+
+#endif
